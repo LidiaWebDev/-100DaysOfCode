36c6c1cea6e54c8fd9146373f9c0c09b953e7307
Lidia Davidson <43212304+LidiaWebDev@users.noreply.github.com>
2019-11-15 09:30:21 -0800

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,21 +1,14 @@
-
- var result; 
- for(var i = 1; i < 101; i++ ) {
-     result= '';
-    if ( [i]%2 == 0)  {
-     result+="Fizz<hr>";
-    }
-    if( [i]%3 == 0 ) {
-    result+="Buzz<hr>";
-    }
-   
-    if([i]%5 == 0) {
-     result+="FizzBuzz<hr>";
-    }
-    else {
-        result+=[i]+ "<hr>";
-        
-    }
-    document.write(result);
-
-   }
+var result;
+for (var i = 1; i < 101; i++) {
+  result = "";
+  if ([i] % 2 == 0) {
+    result += "Fizz<hr>";
+  } else if ([i] % 3 == 0) {
+    result += "Buzz<hr>";
+  } else if ([i] % 5 == 0) {
+    result += "FizzBuzz<hr>";
+  } else {
+    result += [i] + "<hr>";
+  }
+  document.write(result);
+}
